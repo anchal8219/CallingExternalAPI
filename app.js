0c2dd1508c548b5007bfd98313524fe84fc0ae2b
Anchal <anchaljaiswal1341@gmail.com>
2022-07-16 00:12:21 +0530

    I make change in  node-scheduling

--- app.js
+++ app.js
@@ -1,22 +1,15 @@
-// const express=require('express')
 const apiCallFromRequest = require('./Request')
 const apiCallFromNode = require('./NodeJsCall')
 const http= require('http')
-// const app = express()
 const schedule=require('node-schedule')
 
-// app.get('/',(req,res)=>{
-//     const url = 'https://brl1test.herokuapp.com/';
-//     https.get(url,(response)=>{
-//         console.log(response);
-// })
 
 http.createServer((req, res) => {
     if(req.url === "/request"){
         apiCallFromRequest.callApi(function(response){
             res.write(JSON.stringify(response));
             res.end();
-            schedule.scheduleJob('* * * * * *',(req,res)=>{
+            schedule.scheduleJob('0 * * * *',(req,res)=>{
             console.log(response);
 
         });
@@ -34,36 +27,3 @@ http.createServer((req, res) => {
 console.log("service running on 4000 port....");
 
 
-// app.get('/request',(req,res)=>{
-//     const url = 'https://brl1test.herokuapp.com/';
-//     http.get(url,(response)=>{
-//         schedule.scheduleJob('* * * * * *',()=>{
-//         console.log(response);
-// })
-//     })
-// })
-
-
-// schedule.scheduleJob('* * * * * *',(req,res)=>{
-    // console.log('I ran...')
-    // if(req.url === "/request"){
-    //     apiCallFromRequest.callApi(function(response){
-    //         console.log(response);
-    //     //     res.write(JSON.stringify(response));
-    //     //     res.end();
-    //     });
-    // }
-    // schedule.cancelJob('m-job');
-// })
-
-//    OR-
-// const mJob = schedule.scheduleJob('m-job','*/2 * * * * *',()=>{
-//     console.log('I ran...')
-//     mJob.cancel()
-// })
-
-
-
-// app.listen(3000,()=>{
-//     console.log("running ");
-// })
